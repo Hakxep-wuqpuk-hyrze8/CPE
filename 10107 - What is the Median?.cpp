6cda8ec874d21ecbaa422d2de98250d4abfe8b42
0.0 <65276244+Hakxep-wuqpuk-hyrze8@users.noreply.github.com>
2022-07-09 01:29:43 +0800

    Update 10107 - What is the Median?.cpp

--- 10107 - What is the Median?.cpp
+++ 10107 - What is the Median?.cpp
@@ -14,56 +14,22 @@ const int mxN = 100005;
 int dx[8] = {1, 1, 1, 0, -1, -1, -1, 0};
 int dy[8] = {1, 0, -1, -1, -1, 0,  1, 1};
 
-int p[mxN], sz[mxN];
-
-int Find (int x) {
-    return x == p[x] ? x : p[x] = Find(p[x]);
-}
-
-void Union (int x, int y) {
-    int Fx = Find(x), Fy = Find(y);
-
-    if (Fx == Fy) {
-        cout << sz[Fx] << '\n';
-        return;
-    }
-
-    if (sz[Fx] > sz[Fy]) {
-        sz[Fx] += sz[Fy];
-        p[Fy] = Fx;
-        cout << sz[Fx] << '\n';
-    } else {
-        swap(Fx, Fy);
-        sz[Fx] += sz[Fy];
-        p[Fy] = Fx;
-        cout << sz[Fx] << '\n';
-    }
-}
-
 int main()
 {
-    int T; cin >> T;
+    int N;
+    vector<int> v;
 
-    while (T--) {
-        for (int i = 0; i < mxN; i++) p[i] = i;
-        for (int i = 0; i < mxN; i++) sz[i] = 1;
-        int q; cin >> q;
-        int cnt = 1;
-        map<string, int> mp;
-        while (q--) {
-            string s, t; cin >> s >> t;
-            if (!mp[s]) {
-                mp[s] = cnt++;
-            }
-            if (!mp[t]) {
-                mp[t] = cnt++;
-            }
+    while (cin >> N) {
 
-            Union(mp[s], mp[t]);
+        v.insert(lower_bound(all(v), N), N);
+
+        if (v.size() % 2) {
+            cout << v[v.size() / 2] << '\n';
+        } else {
+            cout << ( v[v.size() / 2 - 1] + v[v.size() / 2] ) / 2 << '\n';
         }
+        //for (auto e : v) cout << e << ' ';cout << '\n';
     }
-
-
 
     return 0;
 }

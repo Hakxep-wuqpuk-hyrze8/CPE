d7837c4dda558e3f88ffdc21362f14c8ed1e7c9b
0.0 <65276244+Hakxep-wuqpuk-hyrze8@users.noreply.github.com>
2022-07-14 13:22:15 +0800

    Update 11060 Beverages.cpp

--- 11060 Beverages.cpp
+++ 11060 Beverages.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int mxN = 100005;
+const int mxN = 105;
 
 #define ll long long
 #define pb push_back
@@ -14,56 +14,76 @@ const int mxN = 100005;
 int dx[8] = {1, 1, 1, 0, -1, -1, -1, 0};
 int dy[8] = {1, 0, -1, -1, -1, 0,  1, 1};
 
-int p[mxN], sz[mxN];
-
-int Find (int x) {
-    return x == p[x] ? x : p[x] = Find(p[x]);
-}
-
-void Union (int x, int y) {
-    int Fx = Find(x), Fy = Find(y);
-
-    if (Fx == Fy) {
-        cout << sz[Fx] << '\n';
-        return;
-    }
-
-    if (sz[Fx] > sz[Fy]) {
-        sz[Fx] += sz[Fy];
-        p[Fy] = Fx;
-        cout << sz[Fx] << '\n';
-    } else {
-        swap(Fx, Fy);
-        sz[Fx] += sz[Fy];
-        p[Fy] = Fx;
-        cout << sz[Fx] << '\n';
-    }
-}
+vector<int> adj[mxN];
 
 int main()
 {
-    int T; cin >> T;
-
-    while (T--) {
-        for (int i = 0; i < mxN; i++) p[i] = i;
-        for (int i = 0; i < mxN; i++) sz[i] = 1;
-        int q; cin >> q;
-        int cnt = 1;
+    int n, ts = 1;
+    while (cin >> n) {
+        int in[mxN] = {0};
         map<string, int> mp;
-        while (q--) {
-            string s, t; cin >> s >> t;
-            if (!mp[s]) {
-                mp[s] = cnt++;
-            }
-            if (!mp[t]) {
-                mp[t] = cnt++;
-            }
-
-            Union(mp[s], mp[t]);
+        map<int, string> tag;
+        vector<int> ans;
+        string s;
+        int label = 1;
+        for (int i = 0; i < n; i++) {
+            cin >> s;
+            mp[s] = label;
+            tag[label++] = s;
         }
+
+        int m; cin >> m;
+        for (int i = 0; i < m; i++) {
+            string a, b;
+            cin >> a >> b;
+            adj[mp[a]].pb(mp[b]);
+            in[mp[b]]++;
+        }
+
+        queue<int> q;
+        vector<int> order;
+
+        for (int i = 1; i <= n; i++) {
+            if (in[i] == 0) {
+                if (!q.size()) {
+                    ans.pb(i);
+                    q.push(i);
+                }
+                else {
+                    order.push_back(i);
+                }
+            }
+        }
+
+        while (q.size()) {
+            int p = q.front(); q.pop();
+
+            for (auto e : adj[p]) {
+                in[e]--;
+                if (in[e] == 0) {
+                    order.push_back(e);
+                }
+            }
+
+            if (!order.size()) continue;
+
+            sort(all(order));
+            q.push(order[0]);
+            ans.pb(order[0]);
+
+            order.erase(order.begin());
+        }
+
+        cout << "Case #" << ts++ << ": Dilbert should drink beverages in this order:";
+
+        for (auto e : ans) {
+            cout << ' ' << tag[e];
+        }
+        cout << ".\n\n";
+
+        for (int i = 0; i < mxN; i++) adj[i].clear();
+        ans.clear();
     }
-
-
 
     return 0;
 }
